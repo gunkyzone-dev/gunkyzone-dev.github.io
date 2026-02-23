c631fe938b707c002db5151b5b51852b44e6ceb8
Alan Andres Guajardo Monteforte <gunkyzone@gmail.com>
2026-02-23 15:25:14 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD1L4Mmgnbl0W7lk1rnaRu5wv61TSFhJqM",
+  authDomain: "gunkychat.firebaseapp.com",
+  databaseURL: "https://gunkychat-default-rtdb.firebaseio.com",
+  projectId: "gunkychat",
+  storageBucket: "gunkychat.firebasestorage.app",
+  messagingSenderId: "423489767287",
+  appId: "1:423489767287:web:ce847768781b0f589dc372",
+  measurementId: "G-W7VBG0GSV9"
+};
